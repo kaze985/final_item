a3601107e3679f13c7ad8361fb60dc3b84f67fac
kaze8888 <2082856592@qq.com>
2022-07-27 23:35:51 +0800

    update

--- application/src/main/java/com/example/demo/FinalApplication.java
+++ application/src/main/java/com/example/demo/FinalApplication.java
@@ -3,9 +3,10 @@ package com.example.demo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication(scanBasePackages = {"com.example.demo","com.geekgame.demo"})
+@SpringBootApplication(scanBasePackages = {"com.example.demo","com.geekgame.demo"},exclude = {DataSourceAutoConfiguration.class})
 @MapperScan(basePackages = {"com.geekgame.demo.dao"})
 @EnableDiscoveryClient
 public class FinalApplication {
--- shared.all/item.shared/item.service.impl/src/main/java/com/geekgame/demo/service/impl/ItemServiceImpl.java
+++ shared.all/item.shared/item.service.impl/src/main/java/com/geekgame/demo/service/impl/ItemServiceImpl.java
@@ -7,6 +7,7 @@ import com.geekgame.demo.model.Paging;
 import com.geekgame.demo.param.BasePageParam;
 import com.geekgame.demo.service.ItemService;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.dubbo.config.annotation.DubboService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,6 +17,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
+@DubboService
 public class ItemServiceImpl implements ItemService {
 
     @Autowired
